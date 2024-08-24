15d60acceb9406fa2ee9b23fead45d218fc56249
Manuel Variego <73005152+manuvariego@users.noreply.github.com>
2024-08-24 16:11:44 -0300

    add: getMatchesByPuuid

--- api/handlers.go
+++ api/handlers.go
@@ -2,10 +2,12 @@ package api
 
 import (
 	"net/http"
-  "io"
+  // "io"
   "fmt"
   "os"
   "log"
+  "leagueinform/types"
+  "encoding/json"
 )
 
 // func HandleUserMatches(w http.ResponseWriter, r *http.Request) {
@@ -17,28 +19,53 @@ import (
 
 
 func GetRiotPuuid(w http.ResponseWriter, r *http.Request){
-  var accountName, accountTag string
 
+  account1:= &types.Account{}
 
   riotKey := os.Getenv("RIOT_KEY")
 
-  accountName = "Krazie"
-  accountTag = "LAS"
+  account1.Name = "Krazie"
+  account1.Tag = "LAS"
 
-  accountUrl := fmt.Sprintf(`https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/%s/%s?api_key=%s`, accountName, accountTag, riotKey)
+  accountUrl := fmt.Sprintf(`https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/%s/%s?api_key=%s`, account1.Name, account1.Tag, riotKey)
 
 	resp, err := http.Get(accountUrl)
   if err != nil{
     log.Fatal(err)
   }
 
-  w.Header().Set("Content-Type", "application/json")
+  json.NewDecoder(resp.Body).Decode(&account1)
 
-  io.Copy(w, resp.Body)
+  // w.Header().Set("Content-Type", "application/json")
+  //
+  // io.Copy(w, resp.Body)
+  fmt.Println(account1)
 
-
+  GetRiotMatchesByPuuid(w, r, account1)
+  fmt.Println("test")
 
 }
 
 
+func GetRiotMatchesByPuuid(w http.ResponseWriter, r *http.Request, acc *types.Account) {
+
+  riotKey := os.Getenv("RIOT_KEY")
+
+  accountUrl := fmt.Sprintf(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/%s/ids?start=0&count=20&api_key=%s`,acc.Puuid , riotKey)
+
+  resp, err := http.Get(accountUrl)
+  if err != nil{
+    log.Fatal(err)
+  }
+
+  err = json.NewDecoder(resp.Body).Decode(&acc.Matches)
+  if err != nil{
+    log.Fatal(err)
+  }
+
+  fmt.Println(acc.Matches)
+
+  w.Header().Set("Content-Type", "application/json")
+  json.NewEncoder(w).Encode(acc.Matches)
+}
 
--- types/account.types.go
+++ types/account.types.go
@@ -1,8 +1,13 @@
-package models
+package types
 
 
 type Account struct {
   Name string `json:"name"`  
   Tag  string `json:"tag"`
   Puuid string `json:"puuid"`
+  Matches []string `json:"matches"`
 }
+
+// type Match struct {
+//   MatchId string `json: "matchId"`
+// }
