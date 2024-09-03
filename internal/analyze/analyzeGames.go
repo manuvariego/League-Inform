ec7f9e383bde6dafe80fa319d18a7280566f6c50
manuvariego <73005152+manuvariego@users.noreply.github.com>
2024-09-02 23:16:57 -0300

    optimize: added go routines for matchinfo requests

--- internal/analyze/analyzeGames.go
+++ internal/analyze/analyzeGames.go
@@ -1,53 +1,40 @@
 package analyze
 
-
 import (
-  "leagueinform/internal/types"
-  "leagueinform/internal/riot"
-  "fmt"
-
-
+	"fmt"
+	"leagueinform/internal/riot"
+	"leagueinform/internal/types"
+	"sync"
 )
 
-
-//takes some matches and analyzes them
 func AnalyzeMatches(acc *types.Account, matches []string) int {
-  var matchesWon int
-  for i:=0; i < len(matches); i++ { 
-    match := riot.GetMatchInfo(acc, matches[i])
-    for _, participant:= range match.Info.Participants {
-      if participant.Puuid== acc.Puuid {
-        switch participant.Win  {
-        case true:
-          matchesWon += 1
-          fmt.Println("This is the match", match.MatchId)
-          fmt.Println("You won this match")
-        case false: 
-          fmt.Println("This is the match", match.MatchId)
-          fmt.Println("You lost this match") 
-        }
-      }
-    }
+	var wg sync.WaitGroup
+	var matchesWon int
+	var mu sync.Mutex
 
-  }
-  return matchesWon
+	for i := 0; i < len(matches); i++ {
+		wg.Add(1)
+		go func(matchID string) {
+			defer wg.Done()
+
+			match := riot.GetMatchInfo(acc, matchID)
+			for _, participant := range match.Info.Participants {
+				if participant.Puuid == acc.Puuid {
+					if participant.Win {
+						mu.Lock()
+						matchesWon++
+						mu.Unlock()
+						fmt.Println("This is the match", match.MatchId)
+						fmt.Println("You won this match")
+					} else {
+						fmt.Println("This is the match", match.MatchId)
+						fmt.Println("You lost this match")
+					}
+				}
+			}
+		}(matches[i])
+	}
+
+	wg.Wait()
+	return matchesWon
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- internal/discord/bot.go
+++ internal/discord/bot.go
@@ -2,72 +2,68 @@ package discord
 
 import (
 	"fmt"
+	"leagueinform/internal/analyze"
 	"leagueinform/internal/riot"
 	"leagueinform/internal/types"
-  "leagueinform/internal/analyze"
 	"log"
+
 	"github.com/bwmarrin/discordgo"
 )
 
-//sess is a discord session initialized in main.go
-func DiscordBot( sess *discordgo.Session) {
+// sess is a discord session initialized in main.go
+func DiscordBot(sess *discordgo.Session) {
 
-  //Create a new account 
-  acc := &types.Account{}
-  i := 0
+	//Create a new account
+	acc := &types.Account{}
+	i := 0
 
-  //Adds a handler to the bot
-  sess.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
-    fmt.Println(m.Content)
-    if m.Author.ID == s.State.User.ID {
-      return 
-    }
+	//Adds a handler to the bot
+	sess.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		fmt.Println(m.Content)
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
 
-    if m.Content == "matches please" {
+		if m.Content == "matches please" {
 
-      mess, err := s.ChannelMessageSend(m.ChannelID, "Please enter your riot name and tag, with this format 'riot-name#tag'")
-      if err != nil {
-        log.Fatal(err)
-      }
+			s.ChannelMessageSend(m.ChannelID, "Please enter your riot name and tag, with this format 'riot-name#tag'")
 
-      fmt.Println(mess)
+			//Hardcoded for now because I do not know how to wait for user input before continuing
+			m.Content = "Krazie#LAS"
 
-      //Hardcoded for now because I do not know how to wait for user input before continuing
-      m.Content = "Krazie#LAS"
+			//Divides the content of the message into the name and tag of the account
+			for i < len(m.Content) {
+				if m.Content[i] == '#' {
+					acc.Name = m.Content[:i]
+					acc.Tag = m.Content[i+1:]
+					break
+				}
+				i++
+			}
 
-      //Divides the content of the message into the name and tag of the account 
-      for i < len(m.Content) {
-        if m.Content[i] == '#' {
-          acc.Name = m.Content[:i]
-          acc.Tag = m.Content[i+1:]
-          break
-        }
-        i++
-      }
+			acc.Puuid = riot.GetId(acc)
+			matches := riot.GetMatchesById(acc)
+			matchesWon := analyze.AnalyzeMatches(acc, matches)
 
+			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("You won %d matches out of 20", matchesWon))
+			s.ChannelMessageSend(m.ChannelID, "Want more information?")
+			if m.Content == "yes" {
+				fmt.Println("do stuff")
 
-      acc.Puuid = riot.GetId(acc)
-      matches := riot.GetMatchesById(acc)
-      matchesWon := analyze.AnalyzeMatches(acc, matches)
-      s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("You won %d matches out of 20",matchesWon))
+			}
+		}
 
+		//!!Create function that takes the matches and sends them as a sum of strings (probably xd)
+		// s.ChannelMessageSend(m.ChannelID, "Here are your matches: " + acc.Matches[1])
+	})
 
-      //only printing the first match for now
+	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+	err := sess.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sess.Close()
 
-      //!!Create function that takes the matches and sends them as a sum of strings (probably xd) 
-      // s.ChannelMessageSend(m.ChannelID, "Here are your matches: " + acc.Matches[1])
-    }
-  })
-
-
-
-  sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
-  err := sess.Open()
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer sess.Close()
-
-  fmt.Println("Bot is running")
+	fmt.Println("Bot is running")
 
 }
