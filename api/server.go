dc73535c4c8f3b4d886a7a720936ad998a8f00a4
Manuel Variego <73005152+manuvariego@users.noreply.github.com>
2024-09-07 21:52:06 -0300

    ci: reader & writer concurrency

--- api/server.go
+++ api/server.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 
@@ -19,35 +20,60 @@ import (
 //		},
 //	}
 
-type Disc struct {
-	HeartBeat int `json:"heartbeat_interval"`
+type HeartBeatData struct {
+	HeartBeat float64 `json:"heartbeat_interval"`
 }
 
-type Message struct {
-	Op     int  `json:"op"`
-	Discod Disc `json:"d"`
+type EventPayload struct {
+	OpCode    int           `json:"op"`
+	Data      HeartBeatData `json:"d"`
+	SeqNumber int           `json:"s"`
+	Name      string        `json:"t"`
 }
 
 func ConnectToDiscord() *websocket.Conn {
+	//Returns a discord websocket connection
 	dialer := websocket.DefaultDialer
 	conn, _, err := dialer.Dial("wss://gateway.discord.gg/?v=10&encoding=json", nil)
 	if err != nil {
 		fmt.Println(err)
 	}
-	// messageType, p, err := conn.NextRear()
-	// fmt.Println(messageType)
-	// fmt.Println(string(p))
-	//
+
+	go Reader(conn)
+	// go Writer(conn)
+
 	return conn
+}
+
+func Heartbeat(conn *websocket.Conn, payload EventPayload, seq int) {
+	// heartbeat := payload.Data.HeartBeat
+	// initialTime := heartbeat * (rand.Float64())
+	for {
+
+	}
+
 }
 
 func Reader(conn *websocket.Conn) string {
 	for {
-		m := Message{}
-		messageType, r, err := conn.NextReader()
-		json.NewDecoder(r).Decode(&m)
-		fmt.Println(messageType)
+		m := EventPayload{}
+		_, p, err := conn.ReadMessage()
+		// messageType, r, err := conn.NextReader()
+		json.NewDecoder(bytes.NewReader(p)).Decode(&m)
+		op := m.OpCode
+		seq := m.SeqNumber
+		switch op {
+		case 10:
+			go Heartbeat(conn, m, seq)
+		}
+
+		// go Writer(conn, m)
+
 		fmt.Println(m)
 		fmt.Println(err)
 	}
 }
+
+// func Writer(conn *websocket.Conn, mess Message) {
+// 	w, err := conn.NextWriter
+// }
